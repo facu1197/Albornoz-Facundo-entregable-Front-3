e2589c53f80aad803d09711cbe594e447742d8f0
Facundo Albornoz <48808354+facu1197@users.noreply.github.com>
2023-03-13 23:50:20 -0300

    Add files via upload

---
+++ ctd-fe3-primer-evaluacion-Albornoz-Facundo/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card(props) {
+  console.log(props.data)
+  if (!props.data) return null
+  return (
+    <div>
+      <p>{props.data.name}</p>
+      <p>{props.data.pet}</p>
+    </div>
+  )
+}
+
+export default Card
